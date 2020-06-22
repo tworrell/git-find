4c0abd5d4a980348bddbf921a8f99b04abd9a521
tworrell <tara.worrell@gmail.com>
2020-06-22 16:55:04 -0400

    Updates

---
+++ precache-manifest.f2da7ca082109f78317974a7d237d4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a88b56ab293aba5ef3c4ba2e88fc24f",
+    "url": "/tworrell/git-find/index.html"
+  },
+  {
+    "revision": "bc9fe4e926019ef2f29f",
+    "url": "/tworrell/git-find/static/css/main.ccc8cf52.chunk.css"
+  },
+  {
+    "revision": "4cee39d4108a8fb58f31",
+    "url": "/tworrell/git-find/static/js/2.58b84fa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tworrell/git-find/static/js/2.58b84fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9fe4e926019ef2f29f",
+    "url": "/tworrell/git-find/static/js/main.6ac6665c.chunk.js"
+  },
+  {
+    "revision": "fb03ff37ddaf1a61b026",
+    "url": "/tworrell/git-find/static/js/runtime-main.64bfd9cc.js"
+  }
+]);
