9feee812b657dcf17a38d69e6ef6e5b2187d4209
tworrell <tara.worrell@gmail.com>
2020-06-22 13:23:56 -0400

    got rid of unecessary file and being to build up my app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = 'https://api.github.com/users';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: 'theham3d',
+      name:'',
+      avatar:'',
+      location:'',
+      repos:'',
+      followers: '',
+      following:'',
+      homeUrl:'',
+      notFound:''
+    }
+  }
+  fetchProfile(username) { 
+    let url = `${API}/${username}`;
+    fetch(url)
+      .then((res) => res.json() )
+      .then((data) => {
+        this.setState({
+          username: data.login,
+          name: data.name,
+          avatar: data.avatar_url,
+          location: data.location,
+          repos: data.public_repos,
+          followers: data.followers,
+          following: data.following,
+          homeUrl: data.html_url,
+          notFound: data.message
+        })
+      })
+      .catch((error) => console.log('Oops! . There Is A Problem') )
+  }
 }
+
+React.render(<App />, document.body);
 
 export default App;
